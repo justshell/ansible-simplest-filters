89e5f26170d1d3b06368ae5a946a78d6c23c3c3e
Игорь Ковеньков <ikovenkov@wamba.com>
2025-02-12 17:22:51 +0300

    add FindFilterOne, FindFilterTwo, SubKeyChanger, save_only_dict_keys

--- plugins/filter/compared_change.py
+++ plugins/filter/compared_change.py
@@ -2,7 +2,10 @@
 class FilterModule(object):
     def filters(self):
         return {
-            'compared_change': self.compared_change
+            'compared_change': self.compared_change,
+            'FindFilterOne': self.FindFilterOne,
+            'FindFilterTwo': self.FindFilterTwo,
+            'SubKeyChanger': self.SubKeyChanger
         }
 
     def compared_change(self, origin_string, value_to_compare, if_this_value, value_to_change, result_value):
@@ -10,6 +13,31 @@ class FilterModule(object):
             origin_string=origin_string.replace(value_to_change,result_value)
         return origin_string
 
+    def FindFilterOne(self, origin_list, path_replace_it, path_replace_to, return_key):
+        tresult=[]
+        for item in origin_list:
+            if return_key in item and item[return_key]==True:
+                if path_replace_it!='':
+                    item['path']=item['path'].replace(path_replace_it,path_replace_to)
+                tresult.append(item['path'])
+        return tresult
+
+    def FindFilterTwo(self, origin_list, path_replace_it, path_replace_to, return_key):
+        tresult={}
+        for item in origin_list:
+            if return_key in item and item[return_key]==True:
+                keyname=item['path']
+                if path_replace_it!='':
+                    keyname=item['path'].replace(path_replace_it,path_replace_to)
+                tresult[keyname]={ 'path':item['path'] }
+        return tresult
+
+    def SubKeyChanger(self, origin_dict, subkey_name, new_subkey_value,skip_not_present):
+        new_dict=origin_dict.copy()
+        for item in new_dict.keys():
+            if subkey_name not in new_dict[item] and skip_not_present==False:
+                new_dict[item][subkey_name]=new_subkey_value
+        return new_dict
 
 # compared_change = filter for string. replaces <value_to_change> to <result_value> if <value_to_compare>==<if_this_value>
 # example:
--- plugins/filter/exclude_keys_from_dict.py
+++ plugins/filter/exclude_keys_from_dict.py
@@ -2,7 +2,8 @@
 class FilterModule(object):
     def filters(self):
         return {
-            'exclude_keys_from_dict': self.exclude_keys_from_dict
+            'exclude_keys_from_dict': self.exclude_keys_from_dict,
+            'save_only_dict_keys': self.save_only_dict_keys
         }
 
     def exclude_keys_from_dict(self, origin_dict, exclude_key_list):
@@ -10,4 +11,22 @@ class FilterModule(object):
         for key in origin_dict.keys():
             if key not in exclude_key_list:
                 tresult[key]=origin_dict[key]
+        return tresult
+
+    def save_only_dict_keys(self, origin_dict, save_keylist):
+        tresult={}
+        for key in origin_dict.keys():
+            if key in save_keylist:
+                tresult[key]=origin_dict[key]
+        return tresult
+
+    def replace_subkey_value(self, origin_dict, behaviour, key_name, new_key_value, old_key_value):
+        tresult=origin_dict.copy()
+        if behaviour=='replace_old':
+            for key in tresult.keys():
+                if key_name in tresult[key].keys() and tresult[key][key_name]==old_key_value:
+                    tresult[key][key_name]=new_key_value
+        elif behaviour=='force_place':
+            for key in tresult.keys():
+                tresult[key][key_name]=new_key_value
         return tresult
