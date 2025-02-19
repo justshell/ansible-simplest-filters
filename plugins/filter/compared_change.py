0a61c6bc695683f45ca776aa78c4474bd20c3e81
justshell <120179241+justshell@users.noreply.github.com>
2025-02-20 00:03:06 +0300

    fix and add SubKeyRename,DictKeyRename,DictKeyRenameByList,ExtractKeysFromSubkeysAsList

--- plugins/filter/compared_change.py
+++ plugins/filter/compared_change.py
@@ -5,7 +5,11 @@ class FilterModule(object):
             'compared_change': self.compared_change,
             'FindFilterOne': self.FindFilterOne,
             'FindFilterTwo': self.FindFilterTwo,
-            'SubKeyChanger': self.SubKeyChanger
+            'SubKeyChanger': self.SubKeyChanger,
+            'SubKeyRename': self.SubKeyRename,
+            'DictKeyRename': self.DictKeyRename,
+            'DictKeyRenameByList': self.DictKeyRenameByList,
+            'ExtractKeysFromSubkeysAsList': self.ExtractKeysFromSubkeysAsList
         }
 
     def compared_change(self, origin_string, value_to_compare, if_this_value, value_to_change, result_value):
@@ -32,12 +36,48 @@ class FilterModule(object):
                 tresult[keyname]={ 'path':item['path'] }
         return tresult
 
-    def SubKeyChanger(self, origin_dict, subkey_name, new_subkey_value,skip_not_present):
+    def SubKeyChanger(self, origin_dict, subkey_name, new_subkey_value,insert_if_absent=False):
         new_dict=origin_dict.copy()
         for item in new_dict.keys():
-            if subkey_name not in new_dict[item] and skip_not_present==False:
+            if subkey_name in new_dict[item] or insert_if_absent:
                 new_dict[item][subkey_name]=new_subkey_value
         return new_dict
+
+    def SubKeyRename(self, origin_dict, subkey_name, new_subkey_name):
+        new_dict=origin_dict.copy()
+        for item in new_dict.keys():
+            if subkey_name in new_dict[item]:
+                new_dict[item][new_subkey_name]=new_dict[item][subkey_name]
+                del new_dict[item][subkey_name]
+        return new_dict
+
+    def DictKeyRename(self, origin_dict, key_name, new_key_name):
+        new_dict=origin_dict.copy()
+        if key_name in new_dict:
+            new_dict[new_key_name]=new_dict[key_name]
+            del new_dict[key_name]
+        return new_dict
+
+    def DictKeyRenameByList(self, origin_dict,list1,list2):
+        i=0
+        n=len(list1)
+        new_dict=origin_dict.copy()
+        while i<n:
+            if list1[i] in new_dict.keys():
+                new_dict[list2[i]]=new_dict[list1[i]]
+                del new_dict[list1[i]]
+            i+=1
+        return new_dict
+
+    def ExtractKeysFromSubkeysAsList(self, origin_dict, sub_keylist,ignore_if_missing=True):
+        tresult=[]
+        for item in origin_dict.keys():
+            for item2 in sub_keylist:
+                if item2 in origin_dict[item].keys():
+                    tresult.append(origin_dict[item][item2])
+                elif ignore_if_missing==False:
+                    raise ValueError('"'+item2+'" missing in "'+item+'"')
+        return tresult
 
 # compared_change = filter for string. replaces <value_to_change> to <result_value> if <value_to_compare>==<if_this_value>
 # example:
